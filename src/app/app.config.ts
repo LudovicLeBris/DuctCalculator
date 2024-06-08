8848f00509afe89f73f07cf05cb81ade21f94205
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-06-08 10:17:04 +0200

    refactor(service) change folder organization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
-import { IconRegisterService } from './shared/icon-register.service';
+import { IconRegisterService } from './shared/services/icon-register.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
