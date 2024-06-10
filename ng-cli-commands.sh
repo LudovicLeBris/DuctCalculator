801b35fd04e34d2968d53980d9ea801d75ff1d95
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-06-10 22:40:37 +0200

    build(test) installing, configuring tests routines

--- ng-cli-commands.sh
+++ ng-cli-commands.sh
@@ -8,6 +8,7 @@ Help()
   echo "options :"
   echo "s    serve the project to the localhost:4200 url"
   echo "h    print this help"
+  echo "t    launch test routine"
   echo "c    run commands in the project container folder, useful for ng cli commands or npm install. The command must be between double quotes ("")"
   echo
   echo "By default without arguments, the command is "bash", who grant access to the project folder in container"
@@ -15,13 +16,16 @@ Help()
 
 Command="bash"
 
-while getopts ":hsc:" option; do
+while getopts ":hstc:" option; do
   case $option in
     h)
       Help
       exit;;
     s)
       docker run -it -u $(id -u) --rm -p 4200:4200 -v "$PWD":/app/DuctCalculator --workdir /app/DuctCalculator duct-calculator ng serve --host 0.0.0.0
+      exit;;
+    t)
+      docker run -it -u $(id -u) --rm -p 9876:9876 -v "$PWD":/app/DuctCalculator --workdir /app/DuctCalculator duct-calculator ng test
       exit;;
     c)
       Command=$OPTARG
