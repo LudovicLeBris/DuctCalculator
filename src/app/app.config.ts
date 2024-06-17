bd93492e0c43da151ff6eb9bddf5e7acc2dc69c1
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-06-17 22:45:03 +0200

    fiw(config) change deprecated provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { IconRegisterService } from './services/icon-register.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
+    // importProvidersFrom(HttpClientModule),
     importProvidersFrom(IconRegisterService),
   ]
 };
