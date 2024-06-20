593cb620bf565a2b1b0beaa97d9bda376d77eea4
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-06-20 15:38:56 +0200

    refactor(service) give up subcription pattern

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -2,32 +2,26 @@ import { Injectable } from '@angular/core';
 import { Duct } from '../models/duct/duct.model';
 import { Airflow } from '../models/airflow/airflow.model';
 import { Apd } from '../models/apd/apd.model';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
-  private duct = new Subject<Duct>();
-  duct$ = this.duct.asObservable();
-
-  private airflow = new Subject<Airflow>();
-  airflow$ = this.airflow.asObservable();
-
-  private apd = new Subject<Apd>();
-  apd$ = this.apd.asObservable();
+  duct= new Duct;
+  airflow = new Airflow;
+  apd = new Apd;
 
   constructor() {}
 
-  setDuct(duct: Duct) {
-    this.duct.next(duct);
+  setDuct(duct: Duct): void {
+    this.duct = duct;
   }
 
-  setAirflow(airflow: Airflow) {
-    this.airflow.next(airflow);
+  setAirflow(airflow: Airflow): void {
+    this.airflow = airflow;
   }
 
-  setApd(apd: Apd) {
-    this.apd.next(apd);
+  setApd(apd: Apd): void {
+    this.apd = apd;
   }
 }
